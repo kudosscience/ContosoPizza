d4933d8906f44304cf2606df9dc78b878105e0d3
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2024-12-24 15:25:42 +0000

    Implement CRUD operations

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dbe696678e83d3bee6ec7671a203db0d76a1f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+816edbefb8bac11bf4b092cd20839642bd922887")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
