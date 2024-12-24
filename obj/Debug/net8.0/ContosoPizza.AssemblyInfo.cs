816edbefb8bac11bf4b092cd20839642bd922887
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2024-12-24 15:07:02 +0000

    Add a controller

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e5769d0a89a3f52f406ea2c7fb2550bfbb19c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dbe696678e83d3bee6ec7671a203db0d76a1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
